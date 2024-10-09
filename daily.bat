ddc722ae2bf65d9506ce1343b127dee5054a721b
elprogramadordeltiempo <elprogramadordeltiempo@outlook.com>
2024-10-09 08:33:42 -0500

    Ejemplo .bat para ejecutar interfaces diarias Sql

---
+++ daily.bat
@@ -0,0 +1,14 @@
+REM ==============================================================================
+REM 							COMENTARIOS
+REM Evita que los comandos se muestren en la consola mientras se ejecuta el script
+REM Obtener la fecha actual en formato AAAAMMDD
+REM sqlcmd -S <servidor> -i <ruta_del_sql> -U <usuario> -P "<contraseña>"
+REM sqlcmd -S localhost -i C:\DATA\scripts\daily\daily.sql -E 
+REM -E indica que se utilizará la autenticación de Windows
+REM ==============================================================================
+@echo off
+set fecha=%date:~6,4%%date:~3,2%%date:~0,2% 
+sqlcmd -S localhost -i C:\DATA\scripts\daily\daily.sql -U sa -P "contraseña"
+
+
+
